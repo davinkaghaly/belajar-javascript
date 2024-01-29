569f89b2c257c021494551d3ec70383588ab65e0
davinkaghaly <davinkaghaly12345@gmail.com>
2024-01-29 14:15:55 +0700

    pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,16 @@
+var x  = prompt("masukan angka :");
+// alert ("selamat datang " + nama);
+
+if (x % 2 == 0) {
+    alert(x + " bilangan genap");
+} else if(x % 2 == 1) {
+    alert(x + " bilangan ganjil");
+} else {
+    alert("data bukan angka");
+}
+
+if(confirm("apakah anda manusa?")){
+    location = "https://www.youtube.com/?app=desktop&hl=en";
+} else {
+    alert ("anda harus menyembah allah");
+}
