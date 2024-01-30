f4ef420d46250d2e7c5fdc020ed32eeadf302ec9
davinkaghaly <davinkaghaly12345@gmail.com>
2024-01-30 09:00:00 +0700

    object-dom

--- javascript-Dom/object.js
+++ javascript-Dom/object.js
@@ -1,14 +1,25 @@
+//object
 var siswa = new Object();
-siswa.namaDepan = "davinka ";
-siswa.namaBelakang = "ghaly "
-siswa.alamat = "bandung"
-siswa.namalengkap =  function() {
-    return this.namaDepan + ' ' + this.namaBelakang
+siswa.namaDepan = "davinka";
+siswa.namaBelakang = "ghaly";
+siswa.alamat = "bandung";
+siswa.namaLengkap = function () {
+  return this.namaDepan + " " + this.namaBelakang;
 };
 
+//object JSON
+var siswa2 = {
+  namaDepan: "cahwi",
+  namaBelakang: "bagas",
+  alamat: "bandung",
+  namaLengkap: function () {
+    return this.namaDepan + " " + this.namaBelakang;
+  },
+};
 
-alert("Nama : " + siswa.namalengkap());
-alert("Alamat : " + siswa.alamat());
-// function namalengkap(){
+alert("Nama :" + siswa.namaLengkap());
+alert("Nama : " + siswa2.namaLengkap());
 
-// }
+// function namaLengkap() {
+
+// // }
